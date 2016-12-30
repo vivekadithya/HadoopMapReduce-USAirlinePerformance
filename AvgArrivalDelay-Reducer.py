c9bc4e5e4871ead02b00030db4280703c739d3fa
Vivek Adithya Mohankumar <vivekadithya@users.noreply.github.com>
2016-12-30 12:20:41 -0600

    Update AvgArrivalDelay-Reducer.py

--- AvgArrivalDelay-Reducer.py
+++ AvgArrivalDelay-Reducer.py
@@ -4,20 +4,20 @@ import sys
 current_key = None
 a = []
 for line in sys.stdin:
-key, val = line.split("\t")
-if val != 'ARR_DELAY' and val is not None:
-val = val.strip()
-try:
-val = float(val)
-except:
-continue
-if key == current_key:
-a.append(val)
-else:
-if current_key is not None:
-sys.stdout.write("{0}\t{1}\n".format(current_key, (sum(a)/len(a))))
-a[:]=[]
-current_key = key
-a.append(val)
-if current_key: #ensure the last key is written out
-sys.stdout.write("{0}\t{1}\n".format(current_key, (sum(a)/len(a))))
+  key, val = line.split("\t")
+  if val != 'ARR_DELAY' and val is not None:
+    val = val.strip()
+    try:
+      val = float(val)
+    except:
+      continue
+    if key == current_key:
+      a.append(val)
+    else:
+      if current_key is not None:
+        sys.stdout.write("{0}\t{1}\n".format(current_key, (sum(a)/len(a)))) #Print Flight Number and Avg. Arrival Delay
+        a[:]=[]
+      current_key = key
+      a.append(val)
+if current_key: #Ensure the Last Key is Written Out
+  sys.stdout.write("{0}\t{1}\n".format(current_key, (sum(a)/len(a))))

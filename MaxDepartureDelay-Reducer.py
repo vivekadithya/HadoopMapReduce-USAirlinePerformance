e9879f72fec5535e66023f975f079d44f743ae00
Vivek Adithya Mohankumar <vivekadithya@users.noreply.github.com>
2016-12-30 10:34:07 -0600

    Create MaxDepartureDelay-Reducer.py

---
+++ MaxDepartureDelay-Reducer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+current_key = None
+a = []
+for line in sys.stdin:
+key, val = line.split("\t")
+if val != 'DEP_DELAY' and val is not None:
+val = val.strip()
+try:
+val = float(val)
+except:
+continue
+if key == current_key:
+a.append(val)
+else:
+if current_key is not None:
+sys.stdout.write("{0}\t{1}\n".format(current_key, max(a)))
+a[:]=[]
+current_key = key
+a.append(val)
+if current_key: #ensure the last key is written out
+sys.stdout.write("{0}\t{1}\n".format(current_key, max(a)))

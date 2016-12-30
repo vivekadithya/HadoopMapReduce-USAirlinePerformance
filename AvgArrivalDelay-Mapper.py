4721ad9074b731663fec64ed32d0e2ebe7b92fef
Vivek Adithya Mohankumar <vivekadithya@users.noreply.github.com>
2016-12-30 12:16:06 -0600

    Update AvgArrivalDelay-Mapper.py

--- AvgArrivalDelay-Mapper.py
+++ AvgArrivalDelay-Mapper.py
@@ -2,6 +2,6 @@
 import sys
 
 if __name__ == "__main__":
-for line in sys.stdin:
-a = line.split(",")
-sys.stdout.write("{0}\t{1}\n".format(a[2],a[8]))
+  for line in sys.stdin:
+    a = line.split(",")
+    sys.stdout.write("{0}\t{1}\n".format(a[2],a[8])) #Print Flight Number and Arrival Delay

7f1d2efec15ae9d80e6e0db97d990c8134a1e628
Vivek Adithya Mohankumar <vivekadithya@users.noreply.github.com>
2016-12-30 11:43:44 -0600

    Update MinArrivalDelay-Mapper.py

--- MinArrivalDelay-Mapper.py
+++ MinArrivalDelay-Mapper.py
@@ -2,6 +2,6 @@
 import sys
 
 if __name__ == "__main__":
-for line in sys.stdin:
-a = line.split(",")
-sys.stdout.write("{0}{1}\t{2}\n".format(a[3],a[4],a[8]))
+  for line in sys.stdin:
+    a = line.split(",")
+    sys.stdout.write("{0}{1}\t{2}\n".format(a[3],a[4],a[8])) #print origin airport, destination airport and delay in minutes

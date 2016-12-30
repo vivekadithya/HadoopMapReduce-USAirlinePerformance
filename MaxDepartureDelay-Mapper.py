e2259042a63bc05dc61b3c79c01d9cea61328034
Vivek Adithya Mohankumar <vivekadithya@users.noreply.github.com>
2016-12-30 11:53:25 -0600

    Update MaxDepartureDelay-Mapper.py

--- MaxDepartureDelay-Mapper.py
+++ MaxDepartureDelay-Mapper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
+
 if __name__ == "__main__":
-for line in sys.stdin:
-a = line.split(",")
-sys.stdout.write("{0}\t{1}\n".format(a[3],a[6]))
+  for line in sys.stdin:
+    a = line.split(",")
+    sys.stdout.write("{0}\t{1}\n".format(a[3],a[6])) #Print Origin Airport and Delay in Minutes
